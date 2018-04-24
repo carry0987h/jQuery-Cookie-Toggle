74d9880ad43228ff878d31946faca7e901d8f86d
carry0987h <carry0031449@gmail.com>
2018-04-24 19:22:21 +0800

    Add files via upload

---
+++ js/toggle.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  var $accordionHead = $(".accordion_head");
+
+  //toggle the component with class accordion_body
+  $accordionHead.on("click", function() {
+    var $head = $(this);
+
+    $head.toggleClass("collapsed");
+
+    if ($head.is(".collapsed")) {
+      $head.next(".accordion_body").slideUp(300);
+    } else {
+      $head.next(".accordion_body").slideDown(300);
+    }
+
+    var $collapsedHeads = $accordionHead.filter(".collapsed");
+    
+    Cookies.set(
+      "collapsed_Nodes",
+      $collapsedHeads.map(function(){ return this.getAttribute("node-tag"); }).get().join(''),
+      {
+        expires: 7,
+        path: ''
+      }
+    );
+  });
+});
